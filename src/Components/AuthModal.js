1dfc766bf84c9a4baeba7ce49cda985938fa74e4
Chethan <cchethans14@gamil.com>
2024-10-25 13:31:45 +0530

    initial-commit

--- src/Components/AuthModal.js
+++ src/Components/AuthModal.js
@@ -12,6 +12,7 @@ import Token from './TokenContext'; // Import Token management
 import process from 'process';
 
 const RECAPTCHA_SITE_KEY = process.env.captchaKey; // Replace with your reCAPTCHA site key
+console.log(RECAPTCHA_SITE_KEY);
 const API_URL = BASE_API_URL; // Replace with your API endpoint
 
 const AuthModal = ({ open, onClose, onLoginSuccess }) => {
