f2015900b0ff078621436c3db747e7a82701ea6e
Chethan <cchethans14@gamil.com>
2024-10-25 13:42:53 +0530

    initial-commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 // config.js
 import process from 'process';
-const BASE_API_URL = process.env.baseURL;
+const BASE_API_URL =
+  'https://jewel2024-core-project-381139703931.us-central1.run.app';
 
 export default BASE_API_URL;
